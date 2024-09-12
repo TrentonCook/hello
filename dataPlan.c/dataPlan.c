3e3d50e0a9d3c2d05bf4b08d78f7398517708ac0
TrentonCook <tcook15@huskers.unl.edu>
2024-09-12 00:52:22 +0000

    Wed, Sep 11, 2024, 7:52 PM -05:00

--- dataPlan.c/dataPlan.c
+++ dataPlan.c/dataPlan.c
@@ -11,5 +11,12 @@ data Plans for phones to see limits
 int main(int argc, char **argv){
 
 
+//need prinf("") for reading user input
+// the current day of their plan between 1-30
+//total GB used currently ######.### use for decimals
 
+
+// compute if they are over, under , with in the data limit
+//shouuld inform them of how much if left if any and if they have any left how they can spread it out throught the remainder of the month
+//if theyve ran out tell them that
 }
