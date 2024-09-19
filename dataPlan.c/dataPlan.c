063f48d8235850ab0073d6e71c820af6e67826be
TrentonCook <tcook15@huskers.unl.edu>
2024-09-19 14:21:35 +0000

    Thu, Sep 19, 2024, 9:21 AM -05:00

--- dataPlan.c/dataPlan.c
+++ dataPlan.c/dataPlan.c
@@ -11,14 +11,45 @@ data Plans for phones to see limits
 int main(int argc, char **argv){
 
 
-//need prinf("") for reading user input
-// the current day of their plan between 1-30
-//total GB used currently ######.### use for decimals
+ if(argc != 4) {
+        printf("ERROR: invalid data\n");
+        printf(" Correct data Input: (total gb for the plan) (current day of your plan) (gb used so far)\n");
+                exit(1);
+     } else if(atof(argv[1]) < 0 || atoi(argv[2]) < 0 || atof(argv[3]) < 0) {
+        printf("Error: Inputs cannot be less than 0\n");
+                exit(1);
+     }
 
 
-// compute if they are over, under , with in the data limit
-//shouuld inform them of how much if left if any and if they have any left how they can spread it out throught the remainder of the month
-//if theyve ran out tell them that
+double gbTotal = atof(argv[1]) ;
+int dayCurrent = atoi(argv[2]) ;
+double gbUsed = atof(argv[3]) ;
+double dayAllowance, gbLeft, gbEstimate, gbCurrentUsage , currentExpectedTotal ;
+int daysLeft ;
 
+dayAllowance = gbTotal / 30 ;  //what your allowed to do with your plan a day
+gbLeft = gbTotal - gbUsed ;    //how many GB are left in this current month
+daysLeft = 30 - dayCurrent ;   // how many days are left in your current plan
+gbEstimate = gbLeft / daysLeft ;  // usage a day with what is left to stay in your plan
+gbCurrentUsage = gbUsed / dayCurrent ;  //current usage a day
+currentExpectedTotal = (gbCurrentUsage * 30) - gbTotal ; //total GB over your alloted ammount if you dont change your daily usage
 
+ if(gbUsed >= gbTotal) {   // exceedede aloowance laready
+    printf("%d days used, %d days remaining\n", dayCurrent, daysLeft) ;
+    printf("Average daily use: %.2lf GB/day\n", gbCurrentUsage) ;
+    printf("You have already met your limit for this month.  Looks like you're getting some overage charges...\n") ;
+ }else if(dayAllowance < gbCurrentUsage){ // over your daily usage
+    printf("%d days used, %d days remaining\n", dayCurrent, daysLeft) ;
+    printf("Average daily use: %.2lf GB/day\n", gbCurrentUsage) ;
+    printf("\nYou are EXCEEDING your average daily use (%.2lf GB/day).\n", dayAllowance) ;
+    printf("Continuing this high usage, you'll exceed your data plan by %.2lf GB.\n", currentExpectedTotal) ;
+    printf("To stay below your data plan, use no more than %.2lf GB/day.\n", gbEstimate) ;
+ }else if(dayAllowance >= gbCurrentUsage) {    //under daily usage
+    printf("%d days used, %d days remaining\n", dayCurrent, daysLeft) ;
+    printf("Average daily use: %.2lf GB/day\n", gbCurrentUsage) ;
+    printf("\nYou are at or below your average daily use (%.2lf GB/day).\n", dayAllowance);
+    printf("You can use up to %.2lf GB/day and stay below your data plan limit\n", gbEstimate) ;
+ }
+
+ return 0;
 }
---
+++ retire.c/retire.c
@@ -0,0 +1,43 @@
+/**
+ *
+ *Author: Trenton Cook
+ *2024-09-19
+ *
+ *an amortization table for a 401(k) account.
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char **argv){
+
+if(argc != 6) {
+        printf("ERROR: invalid data\n");
+        printf("Correct data Input: (starting balance) (monthly contribution) (annual rate of return) (annual rate of inflation) (years until retirement)\n");
+                exit(1);
+     } else if(atof(argv[1]) < 0 || atof(argv[2]) < 0 || atof(argv[3]) < 0) {
+        printf("Error: Inputs cannot be less than 0\n");
+                exit(1);
+        } else if(atof(argv[4]) < 0 || atoi(argv[5]) < 0){
+            printf("Error: Inputs cannot be less than 0\n");
+                exit(1);
+        }
+
+double startBal = atof(argv[1]) ;
+double monthlyContrib = atof(argv[2]) ;
+double annualRate = atof(argv[3]) ;
+double annualInflate = atof(argv[4]) ;
+int yearsUntil = atoi(argv[5]) ;
+double monthlyRate = (((1 + annualRate)/(1 + annualInflate))-1)/12;
+
+
+
+// after the start bal * monthly rate = next intrest next bal is + intrest+ monthlyContrib
+
+printf("%lf , %lf, %lf, %lf, %d\n", startBal, monthlyContrib, annualRate, annualInflate, yearsUntil);
+printf("%lf,     %lf,       %lf        \n", monthlyRate, balance,interest);
+
+ return 0;
+}
